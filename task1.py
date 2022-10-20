28c47381a5f18b4e4548aeeeba6d19877a330450
Marian Remoroza <mrremoroza@cpp.edu>
2022-10-20 16:38:24 -0700

    final commit

--- task1.py
+++ task1.py
@@ -36,3 +36,6 @@ madd = np.array([[2, 1, 3, 1, -2, 1], [1, -3, -4, 10, 4, 2], [1, 2, 3, 4, 5, 6],
                 [2, 0, 2, 0, 3, 1], [1, 2, 3, 3, 2, 1], [5, 1, 6, 1, -7, -2]])
 print("\nSum of all elements in a matrix:", np.sum(madd))
 '''Perform a matrix operation of your choice'''
+custom = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12],
+                   [13, 14, 15, 16]])
+print("\nMatrix Reshaping:", custom.reshape((1, 16)))
